5c56c57fefa967d45a253725d3e21cd4d105b39a
Taylor Poe <taylor.poe@gaeaglobal.com>
2026-06-10 21:29:16 -0600

    Updates to how css is imported with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
-      publicPath: isProduction ? './' : '/',
+      publicPath: '/',
       clean: true,
     },
     module: {
